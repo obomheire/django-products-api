a3fb1c617a9df01bd50bc8d08c3145784e98c34e
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-07-06 09:52:09 +0100

    Func base view

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 from . import views
 
+# urlpatterns = [
+#     path("products/", views.ProductListAPIView.as_view()),
+#     path("products/info/", views.product_info),
+#     path("products/<int:product_id>/", views.ProductDetailAPIView.as_view()),
+#     path("orders/", views.OrderListAPIView.as_view()),
+# ]
+
+from django.urls import path
+from . import views
+
 urlpatterns = [
-    path("products/", views.ProductListAPIView.as_view()),
+    path("products/", views.product_list),
     path("products/info/", views.product_info),
-    path("products/<int:product_id>/", views.ProductDetailAPIView.as_view()),
-    path("orders/", views.OrderListAPIView.as_view()),
+    path("products/<int:pk>/", views.product_detail),
+    path("orders/", views.order_list),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,5 @@
 from django.db.models import Max
 from django.shortcuts import get_object_or_404
-from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -8,24 +7,28 @@ from api.models import Order, OrderItem, Product
 from api.serializers import OrderSerializer, ProductInfoSerializer, ProductSerializer
 
 
-class ProductListAPIView(generics.ListAPIView):
-    # queryset = Product.objects.all() # Return all products
-    # queryset = Product.objects.exclude(
-    #     stock__gt=0
-    # )  # Return products that are out of stock i.e where stock is 0
-    queryset = Product.objects.filter(stock__gt=0)  # Return products where stock is > 0
-    serializer_class = ProductSerializer
+@api_view(["GET"])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
-class ProductDetailAPIView(generics.RetrieveAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-    lookup_url_kwarg = "product_id"
+@api_view(["GET"])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
 
 
-class OrderListAPIView(generics.ListAPIView):
-    queryset = Order.objects.prefetch_related("items__product", "user")
-    serializer_class = OrderSerializer
+@api_view(["GET"])
+def order_list(request):
+    # orders = Order.objects.all() # Not efficient for fteching orders and it related records
+    orders = Order.objects.prefetch_related(
+        "items__product", "user"
+    )  # More efficient for fteching orders and it related records
+    serializer = OrderSerializer(orders, many=True)
+    return Response(serializer.data)
 
 
 @api_view(["GET"])
