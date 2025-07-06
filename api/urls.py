65ac222bd3754ce883656d85aa6f1eb8bca7f81b
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-07-06 09:52:45 +0100

    Refactored

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
 from . import views
 
-# urlpatterns = [
-#     path("products/", views.ProductListAPIView.as_view()),
-#     path("products/info/", views.product_info),
-#     path("products/<int:product_id>/", views.ProductDetailAPIView.as_view()),
-#     path("orders/", views.OrderListAPIView.as_view()),
-# ]
-
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path("products/", views.product_list),
     path("products/info/", views.product_info),
