ca3b44a27891c12787f450f528daee569ae0c335
Zack Bello <zackoverflow@Zacks-MacBook-Air.local>
2025-07-06 20:01:49 +0100

    Added uuid to product and order item

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path("products/", views.ProductListAPIView.as_view()),
     path("products/info/", views.product_info),
+    # path("products/<uuid:product_id>/", views.ProductDetailAPIView.as_view()),
     path("products/<int:product_id>/", views.ProductDetailAPIView.as_view()),
     path("orders/", views.OrderListAPIView.as_view()),
     path("user-orders/", views.UserOrderListAPIView.as_view()),
